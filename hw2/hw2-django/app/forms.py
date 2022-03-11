10c834838082b611fe6b4db526b7037f8e1c8e45
Trey Plante <treygp15@gmail.com>
2022-03-10 19:03:04 -0500

    Removed name form.

--- hw2/hw2-django/app/forms.py
+++ hw2/hw2-django/app/forms.py
@@ -2,8 +2,6 @@ from django import forms
 from django.forms import ModelForm
 from app.models import User, Album
 
-class NameForm(forms.Form):
-    your_name = forms.CharField(label='Your name', max_length=100)
 
 
 class UserForm(ModelForm):
